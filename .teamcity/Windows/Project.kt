3f63091acaaaebcee576f445e1a856da3c9746a1
ccptoebeans <>
2026-08-10 15:45:00 +0000

    Change cmake presets for windows CI

--- .teamcity/Windows/Project.kt
+++ .teamcity/Windows/Project.kt
@@ -20,10 +20,10 @@ import jetbrains.buildServer.configs.kotlin.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 import jetbrains.buildServer.configs.kotlin.buildFeatures.provideAwsCredentials
 
-val Debug = CarbonBuildWindows("Debug Windows", "Debug", "nmc-x64-windows-debug")
-val Internal = CarbonBuildWindows("Internal Windows", "Internal", "nmc-x64-windows-internal")
-val TrinityDev = CarbonBuildWindows("TrinityDev Windows", "TrinityDev", "nmc-x64-windows-trinitydev")
-val Release = CarbonBuildWindows("Release Windows", "Release", "nmc-x64-windows-release")
+val Debug = CarbonBuildWindows("Debug Windows", "Debug", "x64-windows-debug")
+val Internal = CarbonBuildWindows("Internal Windows", "Internal", "x64-windows-internal")
+val TrinityDev = CarbonBuildWindows("TrinityDev Windows", "TrinityDev", "x64-windows-trinitydev")
+val Release = CarbonBuildWindows("Release Windows", "Release", "x64-windows-release")
 
 object Project : Project({
     id("Windows")
